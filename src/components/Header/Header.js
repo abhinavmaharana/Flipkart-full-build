0c1e75f1b16b0ff21946664f755537125a80554a
abhinavmaharana <abhinavmaharana.004@gmail.com>
2021-01-07 12:28:38 +0530

    next update

---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingCart";
+import { Button } from '@material-ui/core';
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="main">
+                <img 
+                    className="header__logo"
+                    src="http://img1a.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png"
+                />
+                <p className="content">Explore <span className="explore">Plus</span></p>
+                {/* <img width="10" src="//img1a.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png"></img> */} 
+            </div>
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+            <div className="header__nav">
+                <Button>Login</Button>
+                <div className="header__option">
+                    <span className="header__optionLineOne">More</span>                  
+                </div>
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header_optionLineTwo header__basketCount">
+                        {/* optional tie to get data? */}
+                        0
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Headerbelowbar/Headerbelowbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Headerbelowbar.css';
+
+function Headerbelowbar() {
+    return (
+        <div className="headerbelowbar">
+            
+        </div>
+    )
+}
+
+export default Headerbelowbar
